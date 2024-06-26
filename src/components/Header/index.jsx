334026688a0c1c5600bdab538beb11d4195ed1ba
Theodor Dumitriu <mihaidumitriu@theodors-mbp.home>
2024-06-26 13:22:19 +0200

    feat: added the top navigation with basic styling

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,30 @@
 import "./style.css";
 
+import { Link } from "react-router-dom";
+
 const Header = () => {
-  return <h2>from header</h2>;
+  return (
+    <header className="header">
+      <div className="container d-flex justify-content-space-between align-items-center">
+        <Link to="/">
+          <p className="header__logo">🐱</p>
+        </Link>
+        <nav className="header__nav">
+          <ul className="header__list d-flex">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/kitties">Kitties</Link>
+            </li>
+            <li>
+              <Link to="/loved-kitties">Loved Kitties</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
