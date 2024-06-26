f728a907018682ed085abd337b6232e38df261d6
Theodor Dumitriu <mihaidumitriu@theodors-mbp.home>
2024-06-26 13:19:33 +0200

    feat: added the listing for the cats which are liked

--- src/pages/LovedKitties/index.jsx
+++ src/pages/LovedKitties/index.jsx
@@ -1,7 +1,31 @@
 import "./style.css";
 
+// Importing Components
+import ListingItems from "../../components/ListingItems";
+
+// Importing Custom Hooks
+import { useFetch } from "../../hooks/useFetch";
+import { fetchLovedKitties } from "../../utils/http";
+
 const LovedKitties = () => {
-  return <h1>From LovedKitties</h1>;
+  const {
+    isFetching: loading,
+    error,
+    fetchedData: lovedKitties,
+  } = useFetch(fetchLovedKitties, []);
+
+  if (error) {
+    return <p>{error.message}</p>;
+  }
+
+  return loading ? (
+    <p>The Loved Kitties are coming!!!</p>
+  ) : (
+    <main className="main container">
+      {/* isClickable is false here because on the this listing we don't need the functionnality */}
+      <ListingItems kittyArray={lovedKitties} isClickable={false} />
+    </main>
+  );
 };
 
 export default LovedKitties;
--- src/utils/http.js
+++ src/utils/http.js
@@ -9,3 +9,14 @@ export async function fetchKitties() {
     console.log(error);
   }
 }
+
+export async function fetchLovedKitties() {
+  try {
+    const url =
+      "https://site--cute-cat-backend--yq48zswblh2x.code.run/loved-kitties";
+    const { data } = await axios.get(url);
+    return data.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
